6224f0d86bdcece9d1793f8986574c5eb8602742
Exoniack <55805926+Exoniack@users.noreply.github.com>
2024-12-04 17:18:31 +0100

    add python

--- src/roject/main.py
+++ src/roject/main.py
@@ -1,14 +1,30 @@
-import random
+import numpy as np
+import matplotlib.pyplot as plt
 
-# Génère un nombre aléatoire entre 1 et 10
-nombre_a_deviner = random.randint(1, 10)
+dt = 0.0005
+dy = 0.0005
+k = 10**(-4)
+y_max = 0.04
+t_max = 1
+T0 = 100
 
-# Demande à l'utilisateur de deviner le nombre
-print("Devinez le nombre entre 1 et 10 !")
-essai = int(input("Entrez votre nombre : "))
+def FTCS(dt,dy,t_max,y_max,k,T0):
+    s = k*dt/dy**2
+    y = np.arange(0,y_max+dy,dy)
+    t = np.arange(0,t_max+dt,dt)
+    r = len(t)
+    c = len(y)
+    T = np.zeros([r,c])
+    T[:,0] = T0
+    for n in range(0,r-1):
+        for j in range(1, c-1):
+          T[n+1,j] = T[n,j] + s*(T[n,j-1] - 2*T[n,j] + T[n,j+1])  # as before
+          j = c-1
+          T[n+1, j] = T[n,j] + s*(T[n,j-1] - 2*T[n,j] + T[n,j-1])  # note the last term here
+    return y,T,r,s
 
-# Vérifie si l'utilisateur a deviné correctement
-if essai == nombre_a_deviner:
-    print("Bravo ! Vous avez deviné le bon nombre.")
-else:
-    print(f"Dommage ! Le nombre était {nombre_a_deviner}.")
+y,T,r,s = FTCS(dt,dy,t_max,y_max,k,T0)
+
+plot_times = np.arange(0.01,1.0,0.01)
+for t in plot_times:
+    plt.plot(y,T[t/dt,:])
